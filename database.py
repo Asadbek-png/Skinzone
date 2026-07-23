2ed451b8dcb923f5bcd0272a050f9345c059624f
Asadbek-png <asadismailov360@gmail.com>
2026-07-23 23:07:40 +0500

    Refactor database connection setup with declarative base

--- database.py
+++ database.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "sqlite:///skinzone.db"
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
