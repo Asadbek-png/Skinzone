0e1d1caf9301e28ddd324798d1027e21607bc5a0
Asadbek-png <asadismailov360@gmail.com>
2026-07-24 03:50:43 +0500

    Update User model and import Base from database
    Refactor User model to use Base from database module and add index to id.

--- models.py
+++ models.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True)
-    username = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
+    username = Column(String, nullable=True)
     balance = Column(Integer, default=0)
