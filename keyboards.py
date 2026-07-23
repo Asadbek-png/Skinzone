ef5ccc7c5eddcb6726d2ea680feb4990978a5cd8
Asadbek-png <asadismailov360@gmail.com>
2026-07-23 22:43:49 +0500

    Add main keyboard using ReplyKeyboardMarkup

---
+++ keyboards.py
@@ -0,0 +1,9 @@
+from telegram import ReplyKeyboardMarkup
+
+main_keyboard = ReplyKeyboardMarkup(
+    [
+        ["🎁 Daily Bonus", "🎲 Case"],
+        ["👤 Profil", "🛒 Shop"]
+    ],
+    resize_keyboard=True
+)
